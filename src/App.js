74e6fcce0571da1ef702a1d3e933c82f2bcad4bb
avery011 <avery011@gmail.com>
2024-01-23 13:02:02 -0600

    Update App.js for multiple component entries.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/Landing_Page/LandingPage';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/LandingPage" element={<LandingPage />} />
+                    <Route path="/Sign_Up" element={<Sign_Up />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/instant-consultation" element={<InstantConsultation />} />
 
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
 export default App;
